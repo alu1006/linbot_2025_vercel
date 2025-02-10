a1aedef545b6f8ea8602c0286dca6925afe55456
alu1006 <andy8050802@gmail.com>
2025-02-10 16:28:10 +0800

    delete app.run

--- api/app.py
+++ api/app.py
@@ -8,34 +8,27 @@ import os
 app = Flask(__name__)
 
 # # 環境變數（Vercel 會自動讀取）
-LINE_CHANNEL_ACCESS_TOKEN = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
-LINE_CHANNEL_SECRET = os.getenv("LINE_CHANNEL_SECRET")
 
+line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
+line_handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
 
-# # # 確保變數存在
-# if not LINE_CHANNEL_ACCESS_TOKEN or not LINE_CHANNEL_SECRET:
-#     raise ValueError("❌ 缺少 LINE_CHANNEL_ACCESS_TOKEN 或 LINE_CHANNEL_SECRET，請檢查環境變數！")
-
-line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
-handler = WebhookHandler(LINE_CHANNEL_SECRET)
-
-@app.route("/")
+@app.route('/')
 def home():
-    return f"LINE Bot is running on Vercel!"
+    return 'Hello, World!'
 
-# @app.route("/callback", methods=['POST'])
-# def callback():
-#     signature = request.headers['X-Line-Signature']
-#     body = request.get_data(as_text=True)
-
-#     app.logger.info("Request body: " + body)
-
-#     try:
-#         handler.handle(body, signature)
-#     except InvalidSignatureError:
-#         abort(400)
-
-#     return 'OK'
+@app.route("/webhook", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    # handle webhook body
+    try:
+        line_handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
 
 # @handler.add(MessageEvent, message=TextMessage)
 # def handle_message(event):
